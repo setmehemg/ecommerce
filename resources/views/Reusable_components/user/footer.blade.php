8c7243d2a370f650e0057b8bcdaf72e1be6d72d4
setmehemg <henrique.gewehr@universo.univates.br>
2023-11-14 20:36:36 +0000

    first send

---
+++ resources/views/Reusable_components/user/footer.blade.php
@@ -0,0 +1,137 @@
+<!-- Footer -->
+<footer id="footer" class="page-footer font-small  pt-4" style="background:#762a91;">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">ReKrie</h6>
+        <p>Rekrie fornece produtos customizados em madeira. Desde tábuas de carne à moveis de decoração.</p>
+       
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Produtos - em Dev</h6>
+        <p>
+          <a href="{{url('Shop/Flowers-Fruits-Basket-Set')}}" target="_blank">Tabuas de Carne</a>
+        </p>
+        <p>
+          <a href="{{url('Shop/Dusbin')}}" target="_blank">Mesas</a>
+        </p>
+        <p>
+          <a href="{{url('Shop/Mat')}}" target="_blank">Bancadas</a>
+        </p>
+        <p>
+          <a href="{{url('Shop/Containers')}}" target="_blank">Outros</a>
+        </p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Importante</h6>
+        <p>
+          <a href="{{url('about')}}" target="_blank">Sobre Nós</a>
+        </p>
+       
+        <p>
+          <a href="{{url('Help')}}" target="_blank">Ajuda</a>
+        </p>
+        <p>
+          <a href="{{url('register')}}" target="_blank">Registre-se</a>
+        </p>
+          <p>
+          <a href="{{url('login')}}" target="_blank">Logar</a>
+        </p>
+      </div>
+
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Contato</h6>
+        <p>
+          <i class="fas fa-home mr-3"></i>  Teutônia - RS</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> henrique.gewehr@universo.univates.br</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i>Celular: telefone aqui</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i>Fixo: numero fixo aqui</p>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        <p class="text-center text-md-left">© 2023 Copyright:
+          <a href="#">
+            <strong>rekrie.com.br</strong>
+          </a>
+          <br>
+          <span> Developed by <a href="https://www.univates.br/" target="_blank" style=" font-family: 'Balsamiq Sans', cursive;"><u>Henladson</u></a> </span>
+        </p>
+       
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-5 col-lg-4 ml-lg-0">
+
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a href="" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li  class="list-inline-item">
+              <a href="" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
---
+++ resources/views/components/teamindesktopmode.blade.php
@@ -0,0 +1,46 @@
+<h1 class="black-text" style="font-weight:bold;">Nosso Time</h1>
+    <div  style="border-bottom: 2px solid black;width:100px; "></div>
+    <br>
+                       
+        <p style=" font-family: 'Balsamiq Sans', cursive;"> </p>
+
+      <div class="row team px-5 wow" style="margin-left:15%;"   >
+          
+          <div class="col-md-3" >
+                <div class="card mb-2">
+                    <img class="card-img-top" src="{{asset('assets/img/team/henrique.jpeg')}}"
+                      alt="Card image cap">
+                    <div align="left" class="card-body">
+                      <h6 class="card-title font-weight-bold" style="font-family: 'Josefin Sans', sans-serif;">Henrique</h6>
+                      <p class="card-text" style="font-family: 'Balsamiq Sans', cursive;margin-top:-12px;font-size:10px;"><i> Backend</i></p>
+                  
+                    </div>
+                </div>
+                    
+          </div>
+       
+          <div class="col-md-3" >
+            <div class="card mb-2">
+                <img class="card-img-top" src="{{asset('assets/img/team/larissa.jpeg')}}"
+                  alt="Card image cap">
+                <div align="left" class="card-body">
+                  <h6 class="card-title font-weight-bold" style="font-family: 'Josefin Sans', sans-serif;">Larissa</h6>
+                  <p class="card-text" style="font-family: 'Balsamiq Sans', cursive;margin-top:-12px;font-size:10px;"><i>Frontend</i></p>
+              
+                </div>
+            </div>
+                
+      </div> <div class="col-md-3" >
+        <div class="card mb-2">
+            <img class="card-img-top" src="{{asset('assets/img/team/edson.jpeg')}}"
+              alt="Card image cap">
+            <div align="left" class="card-body">
+              <h6 class="card-title font-weight-bold" style="font-family: 'Josefin Sans', sans-serif;">Edson</h6>
+              <p class="card-text" style="font-family: 'Balsamiq Sans', cursive;margin-top:-12px;font-size:10px;"><i>Analist</i></p>
+            </div>
+        </div>  
+      </div>
+    </div>    
+  </div>   
+</div>
+      
